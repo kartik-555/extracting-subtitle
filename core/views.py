ef23cbd9fc429eef985aeb6f125804eb53fd7876
Kartik <kartikvermaaa01@gmail.com>
2024-09-10 14:07:31 +0530

    some changes

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Video, Subtitle
 from .forms import VideoForm
+import srt
 import os
 
 def home(request):
@@ -26,19 +27,38 @@ class UploadVideoView(View):
     def process_video(self, video_id):
         video = Video.objects.get(id=video_id)
         video_path = video.file.path
-        subtitle_path = f"{video_path}.srt"
+
+        # Split the video path into name and extension
+        base_path, _ = os.path.splitext(video_path)
+        subtitle_path = f"{base_path}.srt"
+        vtt_path = f"{base_path}.vtt"
 
         # Run ccextractor to extract subtitles
         result = subprocess.run(['ccextractor', video_path, '-o', subtitle_path], capture_output=True, text=True)
 
         if result.returncode == 0:
-            Subtitle.objects.create(video=video, language='en', content=subtitle_path)
+            # Convert .srt to .vtt
+            with open(subtitle_path, 'r') as srt_file:
+                srt_content = srt_file.read()
+                subtitles = list(srt.parse(srt_content))
+                vtt_content = "WEBVTT\n\n" + srt.compose(subtitles)
+
+            with open(vtt_path, 'w') as vtt_file:
+                vtt_file.write(vtt_content)
+
+            # Save the subtitle information
+            Subtitle.objects.create(video=video, language='en', content=vtt_path)
+            os.remove(subtitle_path)
         else:
             print(result.returncode, "Error extracting subtitles")
 
 class ListVideosView(View):
     def get(self, request):
-        videos = Video.objects.all()
+        query = request.GET.get('q', '')
+        if query:
+            videos = Video.objects.filter(subtitles__content__icontains=query).distinct()
+        else:
+            videos = Video.objects.all()
         return render(request, 'list_videos.html', {'videos': videos})
 
 class VideoDetailView(DetailView):
